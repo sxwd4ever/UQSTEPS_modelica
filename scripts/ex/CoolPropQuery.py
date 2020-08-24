fba707463f8444a21e4c04c367844f8e22748d7a
sxwd4ever <x.sui@uq.edu.au>
2020-08-24 15:00:19 +1000

    Find out the reason for factor 10; Try to distinguish var at on-design point and off-design

---
+++ scripts/ex/CoolPropQuery.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 17:48:28 2020
+
+@author: uqxsui
+"""
+
+import CoolProp as CP
+
+# P, h -> T ([Pa], [J/kg] -> [degC])
+# result = CP.CoolProp.PropsSI("T", "P", 20e6, 'H', 995164, 'CO2') - 273.15
+
+# P, T -> h ([Pa], [K] -> [J/kg])
+# result = CP.CoolProp.PropsSI("H", "P", 20e6, 'T', 715 + 273.15, 'CO2')
+
+# P, T -> mu (local viscosity)([Pa], [K] -> [mu])
+result = CP.CoolProp.PropsSI("V", "P", 20e6, 'T', 700 + 273.15, 'CO2')
+
+
+print("result=" + str(result))
